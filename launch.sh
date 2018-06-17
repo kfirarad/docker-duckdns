9502e4349b9469b148557051f97e1800b8f2c65c
Miha Filej <miha@filej.net>
2018-06-17 10:32:22 +0200

    Allow disabling IP detection via env variable
    Passing `DUCKDNS_REMOTE_DETECT_IP=true` will disable IP detection within
    the container and will let Duck DNS determine the IP of the caller.

--- launch.sh
+++ launch.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 DUCKDNS_UPDATE_INTERVAL=${DUCKDNS_UPDATE_INTERVAL:-1800}
+DUCKDNS_REMOTE_DETECT_IP=${DUCKDNS_REMOTE_DETECT_IP:-false}
 
 if [ -z "$DUCKDNS_DOMAIN" ]; then
 	echo 'Please supply the $DUCKDNS_DOMAIN environment variable'
@@ -12,16 +13,19 @@ if [ -z "$DUCKDNS_TOKEN" ]; then
 fi
 
 while true; do
-	MACHINE_IP=$(ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $NF}')
-	IP=${DUCKDNS_IP:-$MACHINE_IP}
-	if [ -z "$IP" ]; then
-		echo "Failed to find public IP. Is networking up yet?"
-		exit 1
+	URL="https://www.duckdns.org/update?domains=${DUCKDNS_DOMAIN}&token=${DUCKDNS_TOKEN}"
+	if [ "$DUCKDNS_REMOTE_DETECT_IP" = "false" ]; then
+		MACHINE_IP=$(ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $NF}')
+		IP=${DUCKDNS_IP:-$MACHINE_IP}
+		if [ -z "$IP" ]; then
+			echo "Failed to find public IP. Is networking up yet?"
+			exit 1
+		fi
+
+		echo "Using IP: $IP"
+		URL="${URL}&ip=${IP}"
 	fi
 
-	echo "Using IP: $IP"
-
-	URL="https://www.duckdns.org/update?domains=${DUCKDNS_DOMAIN}&token=${DUCKDNS_TOKEN}&ip=${IP}"
 	echo "Calling URL: $URL"
 	curl -s -k "$URL" & wait
 
